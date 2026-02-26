b1dd4ed3a40acdbb3f8c2bf18ff5e5c3207555bc
femnixx <femnitality@gmail.com>
2026-02-26 14:20:24 +0700

    Editted web

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\HomeController;
 
 Route::get('/sign-up', function () { 
     return Inertia::render('SignUp');
@@ -13,6 +14,4 @@ Route::post('/sign-up', [RegisterController::class, 'store']);
 
 Route::get('/sign-in', [LoginController::class, 'create']);
 
-Route::get('/homepage', function () { 
-    return Inertia::render('Homepage');
-});
+Route::get('/homepage', [HomeController::class, 'index']);
