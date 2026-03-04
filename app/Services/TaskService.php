42d3a36a3d3585a484ded5e9c55975ffb2436056
femnixx <femnitality@gmail.com>
2026-03-04 10:22:20 +0700

    uneloquent way for updating task added, will change later

--- app/Services/TaskService.php
+++ app/Services/TaskService.php
@@ -21,6 +21,14 @@ public function createTask(array $data, User $user)
         return $user->tasks()->create($data);
     });
    }
+    public function updateTask(Task $task, User $user) 
+    {
+    return DB::table('tasks')
+    ->where('id', $task->id)
+    ->where('user_id', $user->id)
+    ->update([
+        'task_name' => $task->task_name
+    ]);
+    }
 
 }
-
